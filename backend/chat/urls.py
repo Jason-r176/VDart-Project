7249b8ae578272465e0f77c5a695c9d90c9a4a1f
Yogeshwaran D <d.yogeshwaran62@gmail.com>
2025-07-03 11:28:38 +0530

    Delete backend directory

--- backend/chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.api_root), 
-    path('messages/', views.MessageList.as_view(), name='message-list'),
- # a simple test endpoint
-]
